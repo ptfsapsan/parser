aeecfd0608260a52994495c5168e41c7d8228550
Metallizzer <Metallizzer@users.noreply.github.com>
2020-10-10 16:52:56 +0700

    upd

--- components/parser/news/KuzbassfmRuParser.php
+++ components/parser/news/KuzbassfmRuParser.php
@@ -4,6 +4,7 @@ namespace app\components\parser\news;
 
 use app\components\Helper;
 use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
 use app\components\parser\ParserInterface;
 use DateTime;
 use DateTimeZone;
@@ -12,9 +13,11 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class KuzbassfmRuParser implements ParserInterface
 {
-    const USER_ID  = 2;
-    const FEED_ID  = 2;
-    const SITE_URL = 'http://kuzbassfm.ru/';
+    const USER_ID    = 2;
+    const FEED_ID    = 2;
+    const SITE_URL   = 'http://kuzbassfm.ru/';
+    const SKIP_FIRST = false; // Пропускать первое предложение, оно совпадает с описанием
+    const ROUND_TIME = false; // Если нужно округлять время до начала часа, как на сайте
 
     protected static $posts = [];
 
@@ -33,17 +36,33 @@ class KuzbassfmRuParser implements ParserInterface
             $tz = new DateTimeZone('Asia/Krasnoyarsk');
             $dt = new DateTime($node->filter('pubDate')->text(), $tz);
 
-            // Если нужно округлить время до начала часа, как на сайте
-            // $dt->setTime($dt->format('H'), 0, 0);
+            if (self::ROUND_TIME) {
+                $dt->setTime($dt->format('H'), 0, 0);
+            }
+
+            $description = array_map('trim', explode('.', $node->filter('description')->text()));
 
             $post = new NewsPost(
                 self::class,
                 $node->filter('title')->text(),
-                $node->filter('description')->text(),
+                $description[0],
                 $dt->setTimezone(new DateTimeZone('UTC'))->format('c'),
                 $node->filter('link')->text(),
                 null
             );
+
+            if ($content = implode('. ', array_slice($description, intval(self::SKIP_FIRST)))) {
+                $post->addItem(
+                    new NewsPostItem(
+                        NewsPostItem::TYPE_TEXT,
+                        $content,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                );
+            }
 
             self::$posts[] = $post;
         });
