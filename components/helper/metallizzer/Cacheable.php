e4b27307d810ee08633c82310b917a2b3fd7e7fd
Metallizzer <Metallizzer@users.noreply.github.com>
2020-10-12 20:19:52 +0700

    Добавлены хэлперы

---
+++ components/helper/metallizzer/Cacheable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\components\helper\metallizzer;
+
+use app\components\Helper;
+use Yii;
+
+/**
+ * Для использования кэширования запросов во время разработки, можно подключить кэш.
+ *
+ * в файл config/console.php в секцию components добавить
+ *
+ *  'parserCache' => [
+ *      'class' => 'yii\caching\FileCache', // Для файлового кэша
+ *      'cachePath' => __DIR__.'/../../parser_cache',
+ *  ],
+ */
+trait Cacheable
+{
+    public static function request(string $url)
+    {
+        $cacheKey = md5(__METHOD__.$url);
+        $cache    = Yii::$app->parserCache;
+
+        if (!$cache || false === ($data = $cache->get($cacheKey))) {
+            $curl = Helper::getCurl();
+            $data = $curl->get($url);
+
+            if ($cache) {
+                $cache->set($cacheKey, $data, 3600);
+            }
+        }
+
+        return $data;
+    }
+}
