8bbae2be744a39dc5f20e7ed81f2057277c54757
ruslan <josanr@gmail.com>
2020-10-14 10:07:28 +0300

    SECREATEIO-12 - уточнил Часовой пояс для даты публикации

--- components/parser/news/InterNovostiParser.php
+++ components/parser/news/InterNovostiParser.php
@@ -66,6 +66,7 @@ class InterNovostiParser implements ParserInterface
     {
         $title = $entityData->filter("title")->text();
         $createDate = new DateTime($entityData->filter("pubDate")->text());
+        $createDate->setTimezone(new \DateTimeZone("UTC"));
         $description = $entityData->filter("description")->text();
         $original = $entityData->filter("link")->text();
 
