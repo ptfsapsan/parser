2a96f618edebd120f907ee1caa5a4d59c515bf32
ruslan <josanr@gmail.com>
2020-10-15 15:40:47 +0300

    SECREATEIO-12 - 1109. ЗВЕЗДА АЛТАЯ

---
+++ components/parser/news/ZvezdaAltaiaParser.php
@@ -0,0 +1,240 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use DateTime;
+use DateTimeZone;
+use DOMNode;
+use Exception;
+use linslin\yii2\curl\Curl;
+use Symfony\Component\DomCrawler\Crawler;
+
+
+class ZvezdaAltaiaParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    const ROOT_SRC = "http://zvezdaaltaya.ru";
+
+    const FEED_SRC = "/feed";
+    const LIMIT = 100;
+
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public static function run(): array
+    {
+        $curl = Helper::getCurl();
+        $posts = [];
+
+        $listSourcePath = self::ROOT_SRC . self::FEED_SRC;
+
+        $listSourceData = $curl->get($listSourcePath);
+
+        $crawler = new Crawler($listSourceData);
+
+        $counter = 0;
+        foreach ($crawler->filter("item") as $item) {
+            try {
+                $node = new Crawler($item);
+                $newsPost = self::inflatePost($node);
+                $posts[] = $newsPost;
+                $counter++;
+                if ($counter >= self::LIMIT) {
+                    break;
+                }
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+                continue;
+            }
+        }
+
+        foreach ($posts as $key => $post) {
+            try {
+                self::inflatePostContent($post, $curl);
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+                unset($posts[$key]);
+                continue;
+            }
+        }
+        return $posts;
+    }
+
+    /**
+     * Собираем исходные данные из ответа API
+     *
+     * @param Crawler $postData
+     *
+     * @return NewsPost
+     * @throws Exception
+     */
+    private static function inflatePost(Crawler $postData): NewsPost
+    {
+        $title = $postData->filter("title")->text();
+        $createDate = new DateTime($postData->filterXPath("item/pubDate")->text());
+        $createDate->setTimezone(new DateTimeZone("UTC"));
+        $original = $postData->filter("link")->text();
+
+        $image = $postData->filter("enclosure");
+        $imageUrl = null;
+        if ($image->count() !== 0) {
+            $imageUrl = $image->attr("url");
+        }
+
+        $description = null;
+
+        $descrContainer = $postData->filter("description")->html();
+        $descrArr = explode(">", $descrContainer);
+        if (isset($descrArr[1])) {
+            $description = trim(explode("<", $descrArr[1])[0]);
+        } else {
+            $description = $descrContainer;
+        }
+
+
+        return new NewsPost(
+            self::class,
+            $title,
+            $description,
+            $createDate->format("Y-m-d H:i:s"),
+            $original,
+            $imageUrl
+        );
+
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param          $curl
+     *
+     * @throws Exception
+     */
+    private static function inflatePostContent(NewsPost $post, Curl $curl)
+    {
+        $url = $post->original;
+
+        $pageData = $curl->get($url);
+        if ($pageData === false) {
+            throw new Exception("Url is wrong? nothing received: " . $url);
+        }
+
+        $crawler = new Crawler($pageData);
+
+        $content = $crawler->filter("div.mg-blog-post-box");
+
+        $header = $content->filter("div.mg-header h1");
+        if ($header->count() !== 0) {
+            self::addHeader($post, $header->text(), 1);
+        }
+
+        $image = $crawler->filter("div.mg-blog-post-box img");
+        if ($image->count() !== 0) {
+            self::addImage($post, $image->attr("src"));
+        }
+
+
+        $body = $content->filter("article");
+
+        /** @var DOMNode $bodyNode */
+        foreach ($body->children() as $bodyNode) {
+            $node = new Crawler($bodyNode);
+            if ($node->matches("p") && !empty(trim($node->text(), "\xC2\xA0"))) {
+                self::addText($post, $node->text());
+                continue;
+            }
+        }
+
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param string   $content
+     * @param int      $level
+     */
+    protected static function addHeader(NewsPost $post, string $content, int $level): void
+    {
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_HEADER,
+                $content,
+                null,
+                null,
+                $level,
+                null
+            ));
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param string   $content
+     */
+    protected static function addImage(NewsPost $post, string $content): void
+    {
+        $content = self::normalizeUrl($content);
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_IMAGE,
+                null,
+                $content,
+                null,
+                null,
+                null
+            ));
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param string   $content
+     */
+    protected static function addText(NewsPost $post, string $content): void
+    {
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_TEXT,
+                $content,
+                null,
+                null,
+                null,
+                null
+            ));
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param string   $content
+     */
+    protected static function addQuote(NewsPost $post, string $content): void
+    {
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_QUOTE,
+                $content,
+                null,
+                null,
+                null,
+                null
+            ));
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    protected static function normalizeUrl(string $content)
+    {
+        return preg_replace_callback('/[^\x20-\x7f]/', function ($match) {
+            return urlencode($match[0]);
+        }, $content);
+    }
+}
+
