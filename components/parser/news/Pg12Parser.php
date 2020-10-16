bc192f7bb1e4423fe8550eca06ed48a90974f189
flanker <flanker@bk.ru>
2020-10-16 19:31:08 +0300

    Pg12Parser - https://pg12.ru

---
+++ components/parser/news/Pg12Parser.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *
+ * @author MediaSfera <info@media-sfera.com>
+ * @author FingliGroup <info@fingli.ru>
+ * @author Vitaliy Moskalyuk <flanker@bk.ru>
+ *
+ * @note Данный код предоставлен в рамках оказания услуг, для выполнения поставленных задач по сбору и обработке данных. Переработка, адаптация и модификация ПО без разрешения правообладателя является нарушением исключительных прав.
+ *
+ */
+
+namespace app\components\parser\news;
+
+
+use app\components\mediasfera\MediasferaNewsParser;
+use app\components\mediasfera\NewsPostWrapper;
+use app\components\parser\ParserInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Pg12Parser extends MediasferaNewsParser implements ParserInterface
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    public const NEWS_LIMIT = 100;
+
+    public const SITE_URL = 'https://pg12.ru';
+    public const NEWSLIST_URL = 'https://pg12.ru/rss';
+
+    //    public const TIMEZONE = '+0000';
+    public const DATEFORMAT = 'D, d M Y H:i:s O';
+
+    public const NEWSLIST_POST = '//rss/channel/item';
+    public const NEWSLIST_TITLE = '//title';
+    public const NEWSLIST_LINK = '//link';
+    public const NEWSLIST_DATE = '//pubDate';
+    public const NEWSLIST_DESC = '//description';
+    public const NEWSLIST_IMG = '//enclosure';
+
+    protected static NewsPostWrapper $post;
+
+    public static function run(): array
+    {
+        $posts = [];
+
+        $listContent = self::getPage(self::NEWSLIST_URL);
+
+        $listCrawler = new Crawler($listContent);
+
+        $listCrawler->filterXPath(self::NEWSLIST_POST)->slice(0, self::NEWS_LIMIT)->each(function (Crawler $node) use (&$posts) {
+
+            self::$post = new NewsPostWrapper();
+
+            self::$post->title = self::getNodeData('text', $node, self::NEWSLIST_TITLE);
+            self::$post->original = self::getNodeData('text', $node, self::NEWSLIST_LINK);
+            self::$post->createDate = self::getNodeDate('text', $node, self::NEWSLIST_DATE);
+            self::$post->description = self::getNodeData('text', $node, self::NEWSLIST_DESC);
+
+            self::$post->image = self::getNodeData('url', $node, self::NEWSLIST_IMG);
+
+            $contentNode = ($node->attr('turbo') == 'true') ? '//turbo:content' : '//yandex:full-text';
+
+
+            $html = html_entity_decode(static::filterNode($node, $contentNode)->html());
+
+            $articleCrawler = new Crawler('<body><div>'.$html.'</div></body>');
+
+            static::parseSection($articleCrawler);
+
+            $posts[] = self::$post->getNewsPost();
+        });
+
+        return $posts;
+    }
+}
