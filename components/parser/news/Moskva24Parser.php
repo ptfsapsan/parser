87926b52daefb66a2e8e5c39aed01b0921c372d6
ruslan <josanr@gmail.com>
2020-10-20 10:34:05 +0300

    SECREATEIO-12 - 1281. МОСКВА 24

---
+++ components/parser/news/Moskva24Parser.php
@@ -0,0 +1,246 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use DateTime;
+use DateTimeZone;
+use DOMNode;
+use Exception;
+use linslin\yii2\curl\Curl;
+use Symfony\Component\DomCrawler\Crawler;
+
+
+class Moskva24Parser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    const ROOT_SRC = "https://www.m24.ru";
+
+    const FEED_SRC = "/news";
+    const LIMIT = 100;
+    const NEWS_PER_PAGE = 24;
+    const EMPTY_DESCRIPTION = "empty";
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public static function run(): array
+    {
+        $curl = Helper::getCurl();
+        $posts = [];
+
+        $counter = 0;
+        for ($pageId = 1; $pageId <= ceil(self::LIMIT / self::NEWS_PER_PAGE); $pageId++) {
+
+            $listSourcePath = self::ROOT_SRC . self::FEED_SRC . "?page=" . $pageId;
+
+            $listSourceData = $curl->get($listSourcePath);
+            if(empty($listSourceData)){
+                throw new Exception("Получен пустой ответ от источника списка новостей: ". $listSourcePath);
+            }
+            $crawler = new Crawler($listSourceData);
+            $items = $crawler->filter("div.b-materials-list ul > li");
+            if($items->count() === 0){
+                throw new Exception("Пустой список новостей в ленте: ". $listSourcePath);
+            }
+            foreach ($items as $newsItem) {
+                try {
+                    $node = new Crawler($newsItem);
+                    $newsPost = self::inflatePost($node);
+                    $posts[] = $newsPost;
+                    $counter++;
+                    if ($counter >= self::LIMIT) {
+                        break 2;
+                    }
+                } catch (Exception $e) {
+                    error_log($e->getMessage());
+                    continue;
+                }
+            }
+        }
+        foreach ($posts as $key => $post) {
+            try {
+                self::inflatePostContent($post, $curl);
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+                unset($posts[$key]);
+                continue;
+            }
+        }
+        return $posts;
+    }
+
+    /**
+     * Собираем исходные данные из ответа API
+     *
+     * @param Crawler $postData
+     *
+     * @return NewsPost
+     * @throws Exception
+     */
+    private static function inflatePost(Crawler $postData): NewsPost
+    {
+        $title = $postData->filter("p > a")->text();
+        $original = self::ROOT_SRC . self::normalizeUrl($postData->filter("p > a")->attr("href"));
+        $imageUrl = null;
+        $image = $postData->filter("p > a img");
+        if ($image->count() !== 0) {
+            $imageUrl = $image->attr("src");
+        }
+
+        $dateString = $postData->filter("p > a")->attr("href");
+        $dateArr = explode("/", $dateString);
+        if (count($dateArr) !== 5) {
+            throw new Exception("Could not parse date string");
+        }
+
+        $dateString = $dateArr[count($dateArr) - 2];
+        $timeStr = $postData->filter("p > span.b-materials-list__date")->text();
+
+        $createDate = DateTime::createFromFormat("dmY H:i", $dateString . " " . $timeStr, new DateTimeZone("+03:00"));
+        if ($createDate === false) {
+            throw new Exception("Could not parse date by format");
+        }
+        $createDate->setTimezone(new DateTimeZone("UTC"));
+
+        $description = self::EMPTY_DESCRIPTION;
+
+        return new NewsPost(
+            self::class,
+            $title,
+            $description,
+            $createDate->format("Y-m-d H:i:s"),
+            $original,
+            $imageUrl
+        );
+
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param          $curl
+     *
+     * @throws Exception
+     */
+    private static function inflatePostContent(NewsPost $post, Curl $curl)
+    {
+        $url = $post->original;
+
+        $pageData = $curl->get($url);
+        if (empty($pageData)) {
+            throw new Exception("Получен пустой ответ от страницы новости: " . $url);
+        }
+
+        $crawler = new Crawler($pageData);
+
+        $body = $crawler->filter("div.b-material.b-material_news div.js-mediator-article");
+
+        if ($body->count() === 0) {
+            throw new Exception("Не найден блок новости в полученой странице: " . $url);
+        }
+
+        /** @var DOMNode $bodyNode */
+        foreach ($body->children() as $bodyNode) {
+            $node = new Crawler($bodyNode);
+
+            if ($node->matches("p") && !empty(trim($node->text(), "\xC2\xA0"))) {
+                if ($post->description === self::EMPTY_DESCRIPTION) {
+                    $post->description = Helper::prepareString($node->text());
+                } else {
+                    self::addText($post, $node->text());
+                }
+                continue;
+            }
+        }
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param string   $content
+     * @param int      $level
+     */
+    protected static function addHeader(NewsPost $post, string $content, int $level): void
+    {
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_HEADER,
+                $content,
+                null,
+                null,
+                $level,
+                null
+            ));
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param string   $content
+     */
+    protected static function addImage(NewsPost $post, string $content): void
+    {
+        $content = self::normalizeUrl($content);
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_IMAGE,
+                null,
+                $content,
+                null,
+                null,
+                null
+            ));
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param string   $content
+     */
+    protected static function addText(NewsPost $post, string $content): void
+    {
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_TEXT,
+                $content,
+                null,
+                null,
+                null,
+                null
+            ));
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param string   $content
+     */
+    protected static function addQuote(NewsPost $post, string $content): void
+    {
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_QUOTE,
+                $content,
+                null,
+                null,
+                null,
+                null
+            ));
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    protected static function normalizeUrl(string $content)
+    {
+        return preg_replace_callback('/[^\x21-\x7f]/', function ($match) {
+            return rawurlencode($match[0]);
+        }, $content);
+    }
+}
+
