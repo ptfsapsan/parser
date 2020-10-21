0f1bf40e9cfc7fa180a6378160c8a4b067d75ce7
jcshoww <corveynreigns@gmai.com>
2020-10-21 13:40:36 +0300

    SECREATEIO-10: Правки ссылок

--- components/parser/news/SredneuralskVolnaParser.php
+++ components/parser/news/SredneuralskVolnaParser.php
@@ -162,12 +162,15 @@ class SredneuralskVolnaParser implements ParserInterface
         //Get non-empty links from nodes
         if (self::isLinkType($node) && self::hasText($node)) {
             $link = self::cleanUrl($node->getAttribute('href'));
-            if (! preg_match('/https/', $link)) {
-                $link = UriResolver::resolve($link, static::SITE_URL);
-            }
-            if ($link && $link !== '' && filter_var($link, FILTER_VALIDATE_URL)) {
-                $linkText = self::hasText($node) ? $node->textContent : null;
-                $post->addItem(new NewsPostItem(NewsPostItem::TYPE_LINK, $linkText, null, $link));
+            if ($link && $link !== '') {
+                if (! preg_match('/http[s]?/', $link)) {
+                    $link = UriResolver::resolve($link, static::SITE_URL);
+                }
+                if (filter_var($link, FILTER_VALIDATE_URL)) {
+                    $linkText = self::hasText($node) ? $node->textContent : null;
+                    $linkText = self::cleanText($linkText);
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_LINK, $linkText, null, $link));
+                }
             }
             return;
         }
