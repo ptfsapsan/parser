fabe35c96298b1ec92d1eb87e3738e20ba536c8f
jcshoww <corveynreigns@gmai.com>
2020-10-21 13:35:35 +0300

    SECREATEIO-10: Мини правка ссылки

--- components/parser/news/StavropolnewsParser.php
+++ components/parser/news/StavropolnewsParser.php
@@ -175,7 +175,7 @@ class StavropolnewsParser implements ParserInterface
         if (self::isLinkType($node) && self::hasText($node)) {
             $link = self::cleanUrl($node->getAttribute('href'));
             if ($link && $link !== '') {
-                if (! preg_match('/https/', $link)) {
+                if (! preg_match('/http[s]?/', $link)) {
                     $link = UriResolver::resolve($link, static::SITE_URL);
                 }
                 if (filter_var($link, FILTER_VALIDATE_URL)) {
