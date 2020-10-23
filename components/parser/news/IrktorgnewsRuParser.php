c6151615b45630d03454033ab97f784ff33072f7
jexme <alexeis@wmstudio.su>
2020-10-23 15:18:08 +0400

    Парс сайта https://irktorgnews.ru

---
+++ components/parser/news/IrktorgnewsRuParser.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\helper\nai4rus\AbstractBaseParser;
+use app\components\helper\nai4rus\PreviewNewsDTO;
+use app\components\parser\NewsPost;
+use DateTimeImmutable;
+use DateTimeZone;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+use Throwable;
+
+class IrktorgnewsRuParser extends AbstractBaseParser
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    protected function getSiteUrl(): string
+    {
+        return 'https://irktorgnews.ru';
+    }
+
+    public function parse(int $minNewsCount = 10, int $maxNewsCount = 50): array
+    {
+        $previewList = $this->getPreviewNewsDTOList($minNewsCount, $maxNewsCount);
+
+        $newsList = [];
+
+        /** @var PreviewNewsDTO $newsPostDTO */
+        foreach ($previewList as $key => $newsPostDTO) {
+            $newsList[] = $this->parseNewsPage($newsPostDTO);
+            $this->getNodeStorage()->removeAll($this->getNodeStorage());
+
+            if ($key % $this->getPageCountBetweenDelay() === 0) {
+                usleep($this->getMicrosecondsDelay());
+            }
+        }
+
+        $this->getCurl()->reset();
+        return $newsList;
+    }
+
+
+    protected function getPreviewNewsDTOList(int $minNewsCount = 10, int $maxNewsCount = 100): array
+    {
+        $previewNewsDTOList = [];
+
+        $uriPreviewPage = UriResolver::resolve('/rss', $this->getSiteUrl());
+
+        try {
+            $previewNewsContent = $this->getPageContent($uriPreviewPage);
+            $previewNewsCrawler = new Crawler($previewNewsContent);
+        } catch (Throwable $exception) {
+            if (count($previewNewsDTOList) < $minNewsCount) {
+                throw new RuntimeException('Не удалось получить достаточное кол-во новостей', null, $exception);
+            }
+        }
+
+        $previewNewsCrawler = $previewNewsCrawler->filterXPath('//item');
+
+        $previewNewsCrawler->each(function (Crawler $newsPreview) use (&$previewNewsDTOList, $maxNewsCount) {
+            if (count($previewNewsDTOList) >= $maxNewsCount) {
+                return;
+            }
+
+            $title = $newsPreview->filterXPath('//title')->text();
+            $uri = $newsPreview->filterXPath('//link')->text();
+
+            $publishedAtString = $newsPreview->filterXPath('//pubDate')->text();
+            $publishedAt = DateTimeImmutable::createFromFormat(DATE_RFC1123, $publishedAtString);
+            $publishedAtUTC = $publishedAt->setTimezone(new DateTimeZone('UTC'));
+
+            $image = null;
+            $imageCrawler = $newsPreview->filterXPath('//enclosure');
+            if ($this->crawlerHasNodes($imageCrawler)) {
+                $image = $imageCrawler->attr('url') ?: null;
+            }
+
+            $previewNewsDTOList[] = new PreviewNewsDTO($uri, $publishedAtUTC, $title, null, $image);
+        });
+
+        $previewNewsDTOList = array_slice($previewNewsDTOList, 0, $maxNewsCount);
+
+        return $previewNewsDTOList;
+    }
+
+    protected function parseNewsPage(PreviewNewsDTO $previewNewsDTO): NewsPost
+    {
+        $description = $previewNewsDTO->getDescription();
+        $uri = $previewNewsDTO->getUri();
+
+        $newsPage = $this->getPageContent($uri);
+
+        $newsPageCrawler = new Crawler($newsPage);
+
+        $contentCrawler = $newsPageCrawler->filter('#content');
+        $this->removeDomNodes($contentCrawler, '//table[contains(@class,"contentpaneopen")][1]');
+        $contentCrawler = $contentCrawler->filterXPath('//table[contains(@class,"contentpaneopen")][1]//td[@valign="top"]');
+
+
+        if ($description && $description !== '') {
+            $previewNewsDTO->setDescription($description);
+        }
+
+        $this->purifyNewsPostContent($contentCrawler);
+
+        $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsDTO);
+
+        return $this->factoryNewsPost($previewNewsDTO, $newsPostItemDTOList);
+    }
+}
