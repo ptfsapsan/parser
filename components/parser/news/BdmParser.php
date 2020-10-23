b326709737802b46ef0e0f85c3997d3def0d396a
idevmans <man1080@yandex.ru>
2020-10-23 16:26:36 +0400

    Парсер сайта https://bdm.ru/

---
+++ components/parser/news/BdmParser.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\helper\nai4rus\AbstractBaseParser;
+use app\components\helper\nai4rus\PreviewNewsDTO;
+use app\components\parser\NewsPost;
+use DateTimeImmutable;
+use DateTimeZone;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+use Throwable;
+
+class BdmParser extends AbstractBaseParser
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    protected function getSiteUrl(): string
+    {
+        return 'https://bdm.ru/';
+    }
+
+    protected function getPreviewNewsDTOList(int $minNewsCount = 10, int $maxNewsCount = 100): array
+    {
+        $previewNewsDTOList = [];
+        $pageNumber = 1;
+
+        while (count($previewNewsDTOList) < $maxNewsCount) {
+            $urn = "/publicacii?page={$pageNumber}";
+            $uriPreviewPage = UriResolver::resolve($urn, $this->getSiteUrl());
+            $pageNumber++;
+
+            try {
+                $previewNewsContent = $this->getPageContent($uriPreviewPage);
+                $previewNewsCrawler = new Crawler($previewNewsContent);
+            } catch (Throwable $exception) {
+                if (count($previewNewsDTOList) < $minNewsCount) {
+                    throw new RuntimeException('Не удалось получить достаточное кол-во новостей', null, $exception);
+                }
+                break;
+            }
+
+            $previewNewsXPath = '//section[@class="publications"]//div[@class="publication"]';
+            $previewNewsCrawler = $previewNewsCrawler->filterXPath($previewNewsXPath);
+
+            $previewNewsCrawler->each(function (Crawler $newsPreview) use (&$previewNewsDTOList) {
+                $titleCrawler = $newsPreview->filterXPath('//a[@class="title"]');
+                $title = $titleCrawler->text();
+                $uri = UriResolver::resolve($titleCrawler->attr('href'), $this->getSiteUrl());
+
+                $description = null;
+                $previewNewsDTOList[] = new PreviewNewsDTO($this->encodeUri($uri), null, $title, $description);
+            });
+        }
+
+        $previewNewsDTOList = array_slice($previewNewsDTOList, 0, $maxNewsCount);
+        return $previewNewsDTOList;
+    }
+
+    protected function parseNewsPage(PreviewNewsDTO $previewNewsDTO): NewsPost
+    {
+        $uri = $previewNewsDTO->getUri();
+        $image = null;
+
+        $newsPage = $this->getPageContent($uri);
+
+        $newsPageCrawler = new Crawler($newsPage);
+        $newsPostCrawler = $newsPageCrawler->filterXPath('//div[contains(@class,"article-wrap")]');
+
+        $mainImageCrawler = $newsPageCrawler->filterXPath('//meta[@property="og:image"]')->first();
+        if ($this->crawlerHasNodes($mainImageCrawler)) {
+            $image = $mainImageCrawler->attr('content');
+        }
+        if ($image !== null && $image !== '') {
+            $image = UriResolver::resolve($image, $uri);
+            $previewNewsDTO->setImage($this->encodeUri($image));
+        }
+
+        $description = $newsPostCrawler->filterXPath('//h2[contains(@class,"subtitle-wrap")]')->text();
+        if($description && $description !== ''){
+            $previewNewsDTO->setDescription($description);
+        }
+
+        $contentCrawler = $newsPostCrawler->filterXPath('//div[contains(@class,"inner-wrap")]')->first();
+        $this->removeDomNodes($contentCrawler,'//div[contains(@class,"mobile-slider")]');
+
+        $this->purifyNewsPostContent($contentCrawler);
+
+        $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsDTO);
+
+        return $this->factoryNewsPost($previewNewsDTO, $newsPostItemDTOList);
+    }
+}
