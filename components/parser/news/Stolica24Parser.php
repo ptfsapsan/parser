886f5dcc6d5d40dac39c7bfb1342d5354c309f7e
idevmans <man1080@yandex.ru>
2020-10-23 15:55:08 +0400

    Парсер сайта https://stolitca24.ru/

---
+++ components/parser/news/Stolica24Parser.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\Helper;
+use app\components\helper\nai4rus\AbstractBaseParser;
+use app\components\helper\nai4rus\PreviewNewsDTO;
+use app\components\parser\NewsPost;
+use DateTimeImmutable;
+use DateTimeZone;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+use Throwable;
+
+class Stolica24Parser extends AbstractBaseParser
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    protected function getSiteUrl(): string
+    {
+        return 'https://stolitca24.ru';
+    }
+
+    protected function getPreviewNewsDTOList(int $minNewsCount = 10, int $maxNewsCount = 100): array
+    {
+        $previewNewsDTOList = [];
+
+        $uriPreviewPage = UriResolver::resolve("/news/rss/", $this->getSiteUrl());
+
+        try {
+            $previewNewsContent = $this->getPageContent($uriPreviewPage);
+            $previewNewsCrawler = new Crawler($previewNewsContent);
+        } catch (Throwable $exception) {
+            if (count($previewNewsDTOList) < $minNewsCount) {
+                throw new RuntimeException('Не удалось получить достаточное кол-во новостей', null, $exception);
+            }
+        }
+
+        $previewNewsCrawler = $previewNewsCrawler->filterXPath('//default:item');
+
+        $previewNewsCrawler->each(function (Crawler $newsPreview) use (&$previewList) {
+            $title = $newsPreview->filterXPath('//default:title')->text();
+            $uri = $newsPreview->filterXPath('//default:link')->text();
+
+            $publishedAtString = $newsPreview->filterXPath('//default:pubDate')->text();
+            $publishedAt = DateTimeImmutable::createFromFormat('D, d M Y H:i:s O', $publishedAtString);
+            $publishedAtUTC = $publishedAt->setTimezone(new DateTimeZone('UTC'));
+
+            $description = null;
+
+            $previewList[] = new PreviewNewsDTO($uri, $publishedAtUTC, $title, $description);
+        });
+
+        $previewNewsDTOList = array_slice($previewList, 0, $maxNewsCount);
+        return $previewNewsDTOList;
+    }
+
+
+    protected function parseNewsPage(PreviewNewsDTO $previewNewsDTO): NewsPost
+    {
+        $uri = $previewNewsDTO->getUri();
+        $image = null;
+
+        $newsPage = $this->getPageContent($uri);
+
+        $newsPageCrawler = new Crawler($newsPage);
+        $newsPostCrawler = $newsPageCrawler->filterXPath('//div[@class="detail-entry"]');
+
+        $mainImageCrawler = $newsPostCrawler->filterXPath('//img[1]');
+        if ($this->crawlerHasNodes($mainImageCrawler)) {
+            $image = $mainImageCrawler->attr('src');
+            $this->removeDomNodes($newsPostCrawler,'//img[1]');
+        }
+        if ($image !== null && $image !== '') {
+            $image = UriResolver::resolve($image, $uri);
+            $previewNewsDTO->setImage(Helper::encodeUrl($image));
+        }
+
+        $previewNewsDTO->setDescription(null);
+
+        $contentCrawler = $newsPostCrawler->filterXPath('//div[@class="detail-entry__text"]');
+
+        $this->removeDomNodes($contentCrawler, '//*/following-sibling::text()[contains(., "Фото:")]');
+        $this->purifyNewsPostContent($contentCrawler);
+
+        $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsDTO);
+
+        return $this->factoryNewsPost($previewNewsDTO, $newsPostItemDTOList);
+    }
+}
