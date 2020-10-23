a313bf327b1006411098c0a9e03db51da5270bb1
flanker <flanker@bk.ru>
2020-10-23 19:41:49 +0300

    Исправлено: Дублирование описания в элементы новости

--- components/parser/news/SovaParser.php
+++ components/parser/news/SovaParser.php
@@ -17,6 +17,10 @@ use app\components\mediasfera\NewsPostWrapper;
 use app\components\parser\ParserInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
+
+/**
+ * @rss_html
+ */
 class SovaParser extends MediasferaNewsParser implements ParserInterface
 {
     public const USER_ID = 2;
@@ -27,16 +31,28 @@ class SovaParser extends MediasferaNewsParser implements ParserInterface
     public const SITE_URL = 'https://sova.info';
     public const NEWSLIST_URL = 'https://sova.info/data/rss/2801/';
 
-    //    public const TIMEZONE = '+0000';
     public const DATEFORMAT = 'D, d M Y H:i:s O';
 
     public const NEWSLIST_POST = '//rss/channel/item';
     public const NEWSLIST_TITLE = '//title';
     public const NEWSLIST_LINK = '//link';
     public const NEWSLIST_DATE = '//pubDate';
-    public const NEWSLIST_DESC = '//description';
     public const NEWSLIST_IMG = '//enclosure';
-    public const NEWSLIST_TEXT = '//yandex:full-text';
+
+    public const ARTICLE_TEXT = '.articleContent .generalContent .justify-content-center div:first-child';
+
+    public const ARTICLE_BREAKPOINTS = [
+        'href' => [
+            '#gallerySlider' => false,
+        ],
+        'src' => [
+            'https://jsn.24smi.net/smi.js' => false,
+        ],
+        'class' => [
+            'content_our-partners' => false,
+            'smi24__informer' => false,
+        ]
+    ];
 
     protected static NewsPostWrapper $post;
 
@@ -56,14 +72,21 @@ class SovaParser extends MediasferaNewsParser implements ParserInterface
 
             self::$post = new NewsPostWrapper();
 
-            self::$post->title = self::getNodeData('text', $node, self::NEWSLIST_TITLE);
+            self::$post->title = html_entity_decode(self::getNodeData('text', $node, self::NEWSLIST_TITLE));
             self::$post->original = self::getNodeData('text', $node, self::NEWSLIST_LINK);
             self::$post->createDate = self::getNodeDate('text', $node, self::NEWSLIST_DATE);
             self::$post->image = self::getNodeData('url', $node, self::NEWSLIST_IMG);
 
-            self::$post->description = self::getNodeData('text', $node, self::NEWSLIST_DESC);
+            $articleContent = self::getPage(self::$post->original);
 
-            static::parseItemContent(self::filterNode($node, self::NEWSLIST_TEXT));
+//            print_r($articleContent);
+
+            if (!empty($articleContent)) {
+
+                $articleCrawler = new Crawler($articleContent);
+
+                self::parse($articleCrawler->filter(self::ARTICLE_TEXT));
+            }
 
             $posts[] = self::$post->getNewsPost();
         });
@@ -72,24 +95,14 @@ class SovaParser extends MediasferaNewsParser implements ParserInterface
     }
 
 
-    protected static function parseItemContent(Crawler $node)
-    {
-        $textParts = explode("\n", $node->html());
-
-        foreach ($textParts as $part) {
-            $text = trim($part, self::CHECK_EMPTY_CHARS);
-
-            if($text) {
-                if(strpos($text, 'Фото:') === 0) {
-                    continue;
-                }
-
-                self::$post->itemText = $text;
-
-                if(!self::$post->description) {
-                    self::$post->description = $text;
-                }
-            }
-        }
-    }
+//    protected static function parseNode(Crawler $node, ?string $filter = null): void
+//    {
+//        $node = static::filterNode($node, $filter);
+//
+//        if(strpos($node->text(), 'Фото:') === 0) {
+//            return;
+//        }
+//
+//        parent::parseNode($node);
+//    }
 }
