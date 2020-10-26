1802fcc8d1ee69861f769b669efb176703b3d834
jexme <alexeis@wmstudio.su>
2020-10-26 13:35:20 +0400

    Парс сайта https://arsenal-otechestva.ru/

---
+++ components/parser/news/ArsenalOtechestvaRuParser.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\helper\nai4rus\AbstractBaseParser;
+use app\components\helper\nai4rus\PreviewNewsDTO;
+use app\components\parser\NewsPost;
+use DateTimeImmutable;
+use DateTimeZone;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+use Throwable;
+
+class ArsenalOtechestvaRuParser extends AbstractBaseParser
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    protected function getSiteUrl(): string
+    {
+        return 'https://arsenal-otechestva.ru/';
+    }
+
+    protected function getPreviewNewsDTOList(int $minNewsCount = 10, int $maxNewsCount = 100): array
+    {
+        $previewNewsDTOList = [];
+
+        $uriPreviewPage = UriResolver::resolve('/index.php?option=com_ninjarsssyndicator&feed_id=1&format=raw', $this->getSiteUrl());
+
+        try {
+            $previewNewsContent = $this->getPageContent($uriPreviewPage);
+            $previewNewsCrawler = new Crawler($previewNewsContent);
+        } catch (Throwable $exception) {
+            if (count($previewNewsDTOList) < $minNewsCount) {
+                throw new RuntimeException('Не удалось получить достаточное кол-во новостей', null, $exception);
+            }
+        }
+
+        $previewNewsCrawler = $previewNewsCrawler->filterXPath('//item');
+
+        $previewNewsCrawler->each(function (Crawler $newsPreview) use (&$previewNewsDTOList, $maxNewsCount) {
+            if (count($previewNewsDTOList) >= $maxNewsCount) {
+                return;
+            }
+
+            $title = $newsPreview->filterXPath('//title')->text();
+            $uri = $newsPreview->filterXPath('//link')->text();
+
+            $publishedAtString = $newsPreview->filterXPath('//pubDate')->text();
+            $publishedAt = DateTimeImmutable::createFromFormat(DATE_RFC1123, $publishedAtString);
+            $publishedAtUTC = $publishedAt->setTimezone(new DateTimeZone('UTC'));
+
+            $image = null;
+            $imageCrawler = $newsPreview->filterXPath('//enclosure');
+            if ($this->crawlerHasNodes($imageCrawler)) {
+                $image = $imageCrawler->attr('url') ?: null;
+            }
+
+            $previewNewsDTOList[] = new PreviewNewsDTO($uri, $publishedAtUTC, $title, null, $image);
+        });
+
+        $previewNewsDTOList = array_slice($previewNewsDTOList, 0, $maxNewsCount);
+
+        return $previewNewsDTOList;
+    }
+
+    protected function parseNewsPage(PreviewNewsDTO $previewNewsDTO): NewsPost
+    {
+        $description = $previewNewsDTO->getDescription();
+        $uri = $previewNewsDTO->getUri();
+
+        $newsPage = $this->getPageContent($uri);
+
+        $newsPageCrawler = new Crawler($newsPage);
+
+        $contentCrawler = $newsPageCrawler->filterXPath('//div[@itemprop="articleBody"]');
+        $this->removeDomNodes($contentCrawler, '//div[contains(@id,"likes")]/following-sibling::*');
+        $this->removeDomNodes($contentCrawler, '//div[contains(@id,"likes")]');
+
+        $image = null;
+
+        $mainImageCrawler = $contentCrawler->filterXPath('//img[1]');
+        if ($this->crawlerHasNodes($mainImageCrawler)) {
+            $image = $mainImageCrawler->attr('src');
+            $this->removeDomNodes($contentCrawler, '//img[1]');
+        }
+
+        if ($image !== null && $image !== '') {
+            $image = $this->encodeUri(UriResolver::resolve($image, $this->getSiteUrl()));
+            $previewNewsDTO->setImage($image);
+        }
+
+        if ($description && $description !== '') {
+            $previewNewsDTO->setDescription($description);
+        }
+
+        $this->purifyNewsPostContent($contentCrawler);
+
+        $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsDTO);
+
+        return $this->factoryNewsPost($previewNewsDTO, $newsPostItemDTOList);
+    }
+}
