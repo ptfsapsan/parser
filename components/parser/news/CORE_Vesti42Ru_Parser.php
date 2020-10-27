143389852174e7c70a84857025beccbd10e34d3a
Roman Goncharenya <goncharenya@gmail.com>
2020-10-27 21:27:26 +0300

    approved

--- components/parser/news/CORE_Vesti42Ru_Parser.php
+++ components/parser/news/CORE_Vesti42Ru_Parser.php
@@ -26,7 +26,7 @@ class CORE_Vesti42Ru_Parser extends ParserCore implements ParserInterface
     // (НЕ ИЗМЕНЯТЬ САМОСТОЯТЕЛЬНО!)
     const FOR_CORE_VERSION = '1.0';
     // дебаг-режим (только для разработки) - выводит информацию о действиях парсера
-    protected const DEBUG = false;
+    protected const DEBUG = 0;
 
     public function __construct()
     {
