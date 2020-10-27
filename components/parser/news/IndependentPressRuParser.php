94e263f12b09f7dbe39c398217a401087b93ac3d
a.uzhova <a.uzhova@zebrains.team>
2020-10-27 11:17:05 +0300

    SECREATEIO-6. 1043. ИА НЕЗАВИСИМАЯ ПРЕССА фикс дубль описания

---
+++ components/parser/news/IndependentPressRuParser.php
@@ -0,0 +1,196 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use DateTime;
+use DateTimeZone;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Парсер новостей с сайта https://independent-press.ru/
+ */
+class IndependentPressRuParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+    const SITE_URL = 'https://independent-press.ru';
+
+    public static function run(): array
+    {
+        $parser = new self();
+        return $parser->parse();
+    }
+
+    public function parse(): array
+    {
+        $posts = [];
+
+        $urlNews = self::SITE_URL . '/lastnews/';
+        $newsPage = $this->getPageContent($urlNews);
+        $newsList = $this->getListNews($newsPage);
+        foreach ($newsList as $newsUrl) {
+            $url = $newsUrl;
+            $contentPage = $this->getPageContent($url);
+            $itemCrawler = new Crawler($contentPage);
+
+            $title = $itemCrawler->filterXPath("//*[@class='story-full group']/h1")->text();
+            $date = $this->getDate($itemCrawler->filterXPath("//*[@class='nav-story']/li")->text());
+            $image = $this->getHeadUrl($itemCrawler->filterXPath("//*[@class='fullimgcenter']")->attr('src'));
+            $description = '';
+            $descriptionSrc = $itemCrawler->filterXPath("//*[@class='full-str']");
+            foreach ($descriptionSrc as $item) {
+                foreach ($item->childNodes as $value) {
+                    if (!$description && $text = $this->clearText($value->nodeValue)) {
+                        $description = $text;
+                    }
+                }
+            }
+
+            $post = new NewsPost(
+                self::class,
+                $title,
+                $description,
+                $date,
+                $url,
+                $image
+            );
+
+            $newContentCrawler = (new Crawler($itemCrawler->filterXPath("//*[@class='full-str']")->html()))->filterXPath('//body');
+
+            foreach ($newContentCrawler as $content) {
+                foreach ($content->childNodes as $childNode) {
+                    $nodeValue = $this->clearText($childNode->nodeValue, [$post->description]);
+
+                    if ($nodeValue == 'Оставить комментарий') {
+                        continue;
+                    }
+
+                    if ($nodeValue && $nodeValue != $post->description) {
+                        $this->addItemPost($post, NewsPostItem::TYPE_TEXT, $nodeValue);
+                    }
+                }
+            }
+
+            $posts[] = $post;
+        }
+
+        return $posts;
+    }
+
+    /**
+     * @param NewsPost $post
+     * @param int $type
+     * @param string|null $text
+     * @param string|null $image
+     * @param string|null $link
+     * @param int|null $headerLevel
+     * @param string|null $youtubeId
+     */
+    protected function addItemPost(NewsPost $post, int $type, string $text = null, string $image = null,
+                                   string $link = null, int $headerLevel = null, string $youtubeId = null): void
+    {
+        $post->addItem(
+            new NewsPostItem(
+                $type,
+                $text,
+                $image,
+                $link,
+                $headerLevel,
+                $youtubeId
+            ));
+    }
+
+    /**
+     *
+     * @param string $url
+     *
+     * @return string
+     */
+    protected function getHeadUrl($url): string
+    {
+        return strpos($url, 'http') === false
+            ? self::SITE_URL . $url
+            : $url;
+    }
+
+    /**
+     *
+     * @param string $date
+     *
+     * @return string
+     */
+    protected function getDate(string $date): string
+    {
+        $newDate = new DateTime($date);
+        $newDate->setTimezone(new DateTimeZone("UTC"));
+        return $newDate->format("Y-m-d H:i:s");
+    }
+
+    /**
+     * Получение списка ссылок на страницы новостей
+     *
+     * @param string $page
+     *
+     * @return array
+     */
+    protected function getListNews(string $page): array
+    {
+        $records = [];
+
+        $crawler = new Crawler($page);
+        $list = $crawler->filterXPath("//*[@class='left_cell']/a[1]");
+        foreach ($list as $item) {
+            $records[] = $item->getAttribute("href");
+        }
+
+        return $records;
+    }
+
+    /**
+     *
+     * @param string $uri
+     *
+     * @return string
+     * @throws RuntimeException|\Exception
+     */
+    private function getPageContent(string $uri): string
+    {
+        $curl = Helper::getCurl();
+
+        $result = $curl->get($uri);
+        $responseInfo = $curl->getInfo();
+
+        $httpCode = $responseInfo['http_code'] ?? null;
+
+        if ($httpCode >= 200 && $httpCode < 400) {
+            return $result;
+        }
+
+        throw new RuntimeException("Не удалось скачать страницу {$uri}, код ответа {$httpCode}");
+    }
+
+    /**
+     *
+     * @param string $text
+     * @param array $search
+     *
+     * @return string
+     */
+    protected function clearText(string $text, array $search = []): string
+    {
+        $text = html_entity_decode($text);
+        $text = strip_tags($text);
+        $text = htmlentities($text);
+        $search = array_merge(["&nbsp;"], $search);
+        $text = str_replace($search, ' ', $text);
+        $text = html_entity_decode($text);
+        return trim($text);
+    }
+
+}
