cfbb41c0bd4b0052c653d2f00b9bef69ab631792
Nai <andrei@wmstudio.su>
2020-10-28 12:10:53 +0300

    feat: parser https://www.biwork.ru

---
+++ components/parser/news/BiworkParser.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\helper\nai4rus\AbstractBaseParser;
+use app\components\helper\nai4rus\PreviewNewsDTO;
+use app\components\parser\NewsPost;
+use DateTimeImmutable;
+use DateTimeZone;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+use Throwable;
+
+class BiworkParser extends AbstractBaseParser
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    protected function getSiteUrl(): string
+    {
+        return 'https://www.biwork.ru';
+    }
+
+    protected function getPreviewNewsDTOList(int $minNewsCount = 10, int $maxNewsCount = 100): array
+    {
+        $previewList = [];
+        $pageNumber = 1;
+
+        while (count($previewList) < $maxNewsCount){
+            $url = "/news?view=_news_simple&page={$pageNumber}";
+            $uriPreviewPage = UriResolver::resolve($url, $this->getSiteUrl());
+
+            try {
+                $previewNewsContent = $this->getPageContent($uriPreviewPage);
+                $previewNewsCrawler = new Crawler($previewNewsContent);
+            } catch (Throwable $exception) {
+                break;
+            }
+
+            $previewNewsCrawler = $previewNewsCrawler->filterXPath('//div[contains(@class,"news--col-1")]');
+            if (!$this->crawlerHasNodes($previewNewsCrawler)) {
+                break;
+            }
+
+            $previewNewsCrawler->each(function (Crawler $newsPreview) use (&$previewList) {
+                $titleCrawler = $newsPreview->filterXPath('//h2/parent::a');
+                $uri = UriResolver::resolve($titleCrawler->attr('href'),$this->getSiteUrl());
+
+                $timezone = new DateTimeZone('Asia/Krasnoyarsk');
+                $publishedAtString = $newsPreview->filterXPath('//time')->text();
+                $publishedAt = DateTimeImmutable::createFromFormat('H:i, d.m.Y', $publishedAtString,$timezone);
+                $publishedAtUTC = $publishedAt->setTimezone(new DateTimeZone('UTC'));
+
+                $preview = null;
+                $previewList[] = new PreviewNewsDTO($uri, $publishedAtUTC, $titleCrawler->text(), $preview);
+            });
+        }
+
+
+        if (count($previewList) < $minNewsCount) {
+            throw new RuntimeException('Не удалось получить достаточное кол-во новостей');
+        }
+
+        $previewList = array_slice($previewList, 0, $maxNewsCount);
+
+        return $previewList;
+    }
+
+    protected function parseNewsPage(PreviewNewsDTO $previewNewsItem): NewsPost
+    {
+        $uri = $previewNewsItem->getUri();
+
+        $newsPage = $this->getPageContent($uri);
+
+        $newsPageCrawler = new Crawler($newsPage);
+        $newsPostCrawler = $newsPageCrawler->filterXPath('//h1/parent::div[contains(@class,"b-article")]');
+
+        $image = null;
+        $mainImageCrawler = $newsPageCrawler->filterXPath('//meta[@property="og:image"]');
+        if ($this->crawlerHasNodes($mainImageCrawler)) {
+            $image = $mainImageCrawler->attr('content');
+        }
+        if ($image !== null && $image !== '') {
+            $previewNewsItem->setImage(UriResolver::resolve($image, $uri));
+        }
+
+        $contentCrawler = $newsPostCrawler;
+        $this->removeDomNodes($contentCrawler, '//div[contains(@class,"news--col-1")]');
+        $this->removeDomNodes($contentCrawler, '//div[contains(@class,"b-article__")]');
+
+        $this->purifyNewsPostContent($contentCrawler);
+
+        $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsItem);
+
+        return $this->factoryNewsPost($previewNewsItem, $newsPostItemDTOList);
+    }
+}
