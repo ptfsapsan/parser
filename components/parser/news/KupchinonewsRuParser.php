a78f1614869ff652328a0fe08f0040053f83aaa0
jexme <alexeis@wmstudio.su>
2020-10-28 15:37:58 +0400

    Парс сайта http://kupchinonews.ru/

---
+++ components/parser/news/KupchinonewsRuParser.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\helper\nai4rus\AbstractBaseParser;
+use app\components\helper\nai4rus\PreviewNewsDTO;
+use app\components\parser\NewsPost;
+use DateTimeImmutable;
+use DateTimeZone;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+use Throwable;
+
+class KupchinonewsRuParser extends AbstractBaseParser
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    protected function getSiteUrl(): string
+    {
+        return 'http://kupchinonews.ru/';
+    }
+
+    protected function getPreviewNewsDTOList(int $minNewsCount = 10, int $maxNewsCount = 100): array
+    {
+        $previewNewsDTOList = [];
+
+        $uriPreviewPage = UriResolver::resolve('/feed', $this->getSiteUrl());
+
+        try {
+            $previewNewsContent = $this->getPageContent($uriPreviewPage);
+            $previewNewsCrawler = new Crawler($previewNewsContent);
+        } catch (Throwable $exception) {
+            if (count($previewNewsDTOList) < $minNewsCount) {
+                throw new RuntimeException('Не удалось получить достаточное кол-во новостей', null, $exception);
+            }
+        }
+
+        $previewNewsCrawler = $previewNewsCrawler->filterXPath('//item');
+
+        $previewNewsCrawler->each(function (Crawler $newsPreview) use (&$previewNewsDTOList, $maxNewsCount) {
+            if (count($previewNewsDTOList) >= $maxNewsCount) {
+                return;
+            }
+
+            $title = $newsPreview->filterXPath('//title')->text();
+            $uri = $newsPreview->filterXPath('//link')->text();
+
+            $publishedAtString = $newsPreview->filterXPath('//pubDate')->text();
+            $publishedAt = DateTimeImmutable::createFromFormat(DATE_RFC1123, $publishedAtString);
+            $publishedAtUTC = $publishedAt->setTimezone(new DateTimeZone('UTC'));
+
+            $image = null;
+            $imageCrawler = $newsPreview->filterXPath('//enclosure');
+            if ($this->crawlerHasNodes($imageCrawler)) {
+                $image = $imageCrawler->attr('url') ?: null;
+            }
+
+            $previewNewsDTOList[] = new PreviewNewsDTO($uri, $publishedAtUTC, $title, null, $image);
+        });
+
+        $previewNewsDTOList = array_slice($previewNewsDTOList, 0, $maxNewsCount);
+
+        return $previewNewsDTOList;
+    }
+
+    protected function parseNewsPage(PreviewNewsDTO $previewNewsDTO): NewsPost
+    {
+        $description = $previewNewsDTO->getDescription();
+        $uri = rawurldecode($previewNewsDTO->getUri());
+
+        $newsPage = $this->getPageContent($uri);
+
+        $newsPageCrawler = new Crawler($newsPage);
+        $contentCrawler = $newsPageCrawler->filter('.post .entry-content');
+        $this->removeDomNodes($contentCrawler, '//div[contains(@class,"related-posts")]');
+        $this->removeDomNodes($contentCrawler, '//div[contains(@class,"g-single")]');
+
+        $image = null;
+        $mainImageCrawler = $newsPageCrawler->filterXPath('//div[contains(@class,"featured-image")]/img[1]')->first();
+        if ($this->crawlerHasNodes($mainImageCrawler)) {
+            $image = $mainImageCrawler->attr('src');
+            $this->removeDomNodes($contentCrawler, '//div[contains(@class,"featured-image")]');
+        }
+        if ($image !== null && $image !== '') {
+            $image = UriResolver::resolve($image, $uri);
+            $previewNewsDTO->setImage($this->encodeUri($image));
+        }
+
+        if ($description && $description !== '') {
+            $previewNewsDTO->setDescription($description);
+        }
+
+        $this->purifyNewsPostContent($contentCrawler);
+
+        $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsDTO);
+
+        return $this->factoryNewsPost($previewNewsDTO, $newsPostItemDTOList);
+    }
+}
