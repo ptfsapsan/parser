672b707d7f1597d800371bd7ca7ba5729579f52d
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-09 13:14:09 +0400

    парсер сайта http://www.shakhty.su

---
+++ components/parser/news/ShakhtyParser.php
@@ -0,0 +1,200 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class ShakhtyParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'http://www.shakhty.su/news/';
+    private const DOMAIN = 'http://www.shakhty.su';
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('#tablenta td.tdcherdak');
+        if (count($items)) {
+            foreach ($items as $item) {
+                try {
+                    $item = PhpQuery::pq($item);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $a = $item->find('div a.newsheader');
+                $title = $a->text();
+                $original = $a->attr('href');
+                $original = sprintf('%s%s', self::DOMAIN, $original);
+                $image = $item->find('.anons img')->attr('src');
+                $image = empty($image) ? null : sprintf('%s%s', self::DOMAIN, $image);
+                $description = sprintf('%s %s', trim($title), trim($item->find('.anons')->text()));
+                $originalParser = self::getParser($original, $curl);
+
+
+                $createDate = $originalParser->find('td div font b nobr')->text();
+                $createDate = date('d.m.Y H:i:s', self::getTimestampFromString($createDate));
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        $content = mb_convert_encoding(($content), 'UTF-8', 'KOI8-R');
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $paragraphs = $parser->find('.nativeroll p');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                $text = trim($paragraph->textContent);
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+        $images = $parser->find('.nativeroll p img');
+        if (count($images)) {
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (!empty($src)) {
+                    $src = sprintf('%s%s', self::DOMAIN, urlencode($src));
+                    if (filter_var($src, FILTER_VALIDATE_URL)) {
+                        $post->addItem(
+                            new NewsPostItem(
+                                NewsPostItem::TYPE_IMAGE,
+                                null,
+                                $src,
+                            )
+                        );
+                    }
+                }
+            }
+        }
+        $links = $parser->find('.nativeroll p a');
+        if (count($links)) {
+            foreach ($links as $link) {
+                $href = $link->getAttribute('href');
+                if (!empty($href) && filter_var($href, FILTER_VALIDATE_URL)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_LINK,
+                            null,
+                            null,
+                            $href,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+    private static function getTimestampFromString(string $time): string
+    {
+        if (!preg_match('/^(\d+) (\w+) (\d+)$/ui', trim(mb_strtolower($time)), $matches)) {
+            return time();
+        }
+        switch ($matches[2]) {
+            case 'янв':
+            case 'январь':
+            case 'января':
+                $month = '01';
+                break;
+            case 'фев':
+            case 'февраль':
+            case 'февраля':
+                $month = '02';
+                break;
+            case 'мар':
+            case 'март':
+            case 'марта':
+                $month = '03';
+                break;
+            case 'апр':
+            case 'апрель':
+            case 'апреля':
+                $month = '04';
+                break;
+            case 'май':
+            case 'мая':
+                $month = '05';
+                break;
+            case 'июн':
+            case 'июнь':
+            case 'июня':
+                $month = '06';
+                break;
+            case 'июл':
+            case 'июль':
+            case 'июля':
+                $month = '07';
+                break;
+            case 'авг':
+            case 'август':
+            case 'августа':
+                $month = '08';
+                break;
+            case 'сен':
+            case 'сентябрь':
+            case 'сентября':
+                $month = '09';
+                break;
+            case 'окт':
+            case 'октябрь':
+            case 'октября':
+                $month = '10';
+                break;
+            case 'ноя':
+            case 'ноябрь':
+            case 'ноября':
+                $month = '11';
+                break;
+            case 'дек':
+            case 'декабрь':
+            case 'декабря':
+                $month = '12';
+                break;
+            default:
+                $month = '01';
+        }
+        $time = strtotime(sprintf('%d-%d-%d %s', $matches[3], $month, $matches[1], date('H:i:s')));
+
+        return $time ?? time();
+    }
+
+}
