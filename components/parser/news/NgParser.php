49d59c5df367002f1148b74178adcfbaec757187
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-09 17:15:44 +0400

    парсер сайта https://www.ng.ru

---
+++ components/parser/news/NgParser.php
@@ -0,0 +1,135 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class NgParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'https://www.ng.ru/rss/';
+    private const DOMAIN = 'https://www.ng.ru';
+    private const COUNT = 10;
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('item');
+        if (count($items)) {
+            $n = 0;
+            foreach ($items as $item) {
+                if ($n >= self::COUNT) {
+                    break;
+                }
+                $title = $original = $description = $createDate = $image = null;
+                foreach ($item->childNodes as $childNode) {
+                    switch ($childNode->nodeName) {
+                        case 'title':
+                            $title = trim($childNode->textContent);
+                            break;
+                        case 'link':
+                            $original = trim($childNode->nextSibling->textContent);
+                            break;
+                        case 'description':
+                            $description = trim($childNode->textContent);
+                            break;
+                        case 'pubdate':
+                            $createDate = date('d.m.Y H:i:s', strtotime($childNode->textContent));
+                            break;
+                    }
+                }
+                $originalParser = self::getParser($original, $curl);
+                $image = $originalParser->find('.news_detail_content img:first')->attr('src');
+                $image = empty($image) ? null : sprintf('%s%s', self::DOMAIN, $image);
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+                $n++;
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $paragraphs = $parser->find('.news_detail_content p');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                $text = trim($paragraph->textContent);
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+        $images = $parser->find('.news_detail_content img');
+        if (count($images)) {
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (!empty($src)) {
+                    $src = sprintf('%s%s', self::DOMAIN, $src);
+                    if (filter_var($src, FILTER_VALIDATE_URL)) {
+                        $post->addItem(
+                            new NewsPostItem(
+                                NewsPostItem::TYPE_IMAGE,
+                                null,
+                                $src,
+                            )
+                        );
+                    }
+                }
+            }
+        }
+        $links = $parser->find('.news_detail_content p a');
+        if (count($links)) {
+            foreach ($links as $link) {
+                $href = $link->getAttribute('href');
+                if (!empty($href) && filter_var($href, FILTER_VALIDATE_URL)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_LINK,
+                            null,
+                            null,
+                            $href,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+}
