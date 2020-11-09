1faf849224340051831465b88667eb11f8a55ece
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-09 10:29:54 +0400

    парсер сайта https://smi8.info

---
+++ components/parser/news/Smi8Parser.php
@@ -0,0 +1,181 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class Smi8Parser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'https://smi8.info/';
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('ul.base-content__list li.base-content__item');
+        if (count($items)) {
+            foreach ($items as $item) {
+                try {
+                    $item = PhpQuery::pq($item);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $original = $item->find('.item__preview-wrapper a')->attr('href');
+                $image = $item->find('.item__preview-wrapper a img')->attr('src');
+                if (empty($image) || !filter_var($image, FILTER_VALIDATE_URL)) {
+                    $image = null;
+                }
+                $title = $item->find('h3.item__title a')->text();
+                $createDate = $item->find('p.item__date')->text();
+                $createDate = sprintf('%s %s', date('d.m.Y', self::getTimestampFromString($createDate)), date('H:i:s'));
+                $originalParser = self::getParser($original, $curl);
+                $description = $originalParser->find('.post-content h3')->text();
+                if (empty($description)) {
+                    $description = $title;
+                }
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $paragraphs = $parser->find('.post-content > p:not([style="text-align: center;"])');
+        $paragraphs->find('span')->remove();
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                $text = trim($paragraph->textContent);
+                $text = str_replace('Читайте также:', '', $text);
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+        $images = $parser->find('.post-content p img');
+        if (count($images)) {
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (!empty($src) && filter_var($src, FILTER_VALIDATE_URL)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_IMAGE,
+                            null,
+                            $src,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+    private static function getTimestampFromString(string $time): string
+    {
+        if (!preg_match('/^(\d+) (\w+)$/ui', trim($time), $matches)) {
+            return time();
+        }
+        switch ($matches[2]) {
+            case 'янв':
+            case 'январь':
+            case 'января':
+                $month = '01';
+                break;
+            case 'фев':
+            case 'февраль':
+            case 'февраля':
+                $month = '02';
+                break;
+            case 'мар':
+            case 'март':
+            case 'марта':
+                $month = '03';
+                break;
+            case 'апр':
+            case 'апрель':
+            case 'апреля':
+                $month = '04';
+                break;
+            case 'май':
+            case 'мая':
+                $month = '05';
+                break;
+            case 'июн':
+            case 'июнь':
+            case 'июня':
+                $month = '06';
+                break;
+            case 'июл':
+            case 'июль':
+            case 'июля':
+                $month = '07';
+                break;
+            case 'авг':
+            case 'август':
+            case 'августа':
+                $month = '08';
+                break;
+            case 'сен':
+            case 'сентябрь':
+            case 'сентября':
+                $month = '09';
+                break;
+            case 'окт':
+            case 'октябрь':
+            case 'октября':
+                $month = '10';
+                break;
+            case 'ноя':
+            case 'ноябрь':
+            case 'ноября':
+                $month = '11';
+                break;
+            case 'дек':
+            case 'декабрь':
+            case 'декабря':
+                $month = '12';
+                break;
+            default:
+                $month = '01';
+        }
+        $time = strtotime(sprintf('%d-%d-%d', $matches[1], $month, date('Y')));
+
+        return $time ?? time();
+    }
+}
