562e255edd11a2dfd45580dc92e99eca39e5293b
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-09 16:20:12 +0400

    парсер сайта https://eadaily.com

---
+++ components/parser/news/EaDailyParser.php
@@ -0,0 +1,151 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class EaDailyParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'https://eadaily.com/ru/rss/index.xml';
+    private const DOMAIN = 'https://eadaily.com';
+    private const COUNT = 10;
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('item');
+        if (count($items)) {
+            $n = 0;
+            foreach ($items as $item) {
+                if ($n >= self::COUNT) {
+                    break;
+                }
+                try {
+                    $item = PhpQuery::pq($item);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $title = trim($item->find('title')->text());
+                $original = trim($item->find('link')->text());
+                $createDate = $item->find('pubDate')->text();
+                $description = trim(($item->find('description')->text()));
+                $description = str_replace('...', '', $description);
+                $createDate = date('d.m.Y H:i:s', strtotime($createDate));
+                $originalParser = self::getParser($original, $curl);
+                $image = $originalParser->find('figure.oneimage img:first')->attr('src');
+                if (strpos($image, '//') === 0) {
+                    $image = sprintf('https:%s', $image);
+                }
+                if (empty($image) || !filter_var($image, FILTER_VALIDATE_URL)) {
+                    $image = null;
+                }
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+                $n++;
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $body = $parser->find('.news-text-body');
+        $header = $body->find('p.lead')->text();
+        if (!empty($header)) {
+            $post->addItem(
+                new NewsPostItem(
+                    NewsPostItem::TYPE_HEADER,
+                    trim($header),
+                    null,
+                    null,
+                    1,
+                )
+            );
+        }
+        $body->find('p.lead')->remove();
+        $paragraphs = $body->find('p');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                $text = trim($paragraph->textContent);
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+        $images = $paragraphs->find('img');
+        if (count($images)) {
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (!empty($src)) {
+                    $src = sprintf('https:%s', $src);
+                    if (filter_var($src, FILTER_VALIDATE_URL)) {
+                        $post->addItem(
+                            new NewsPostItem(
+                                NewsPostItem::TYPE_IMAGE,
+                                null,
+                                $src,
+                            )
+                        );
+                    }
+                }
+            }
+        }
+        $links = $paragraphs->find('a');
+        if (count($links)) {
+            foreach ($links as $link) {
+                $href = $link->getAttribute('href');
+                if (!empty($href)) {
+                    if (strpos($href, 'http') === false) {
+                        $href = sprintf('%s%s', self::DOMAIN, $href);
+                    }
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_LINK,
+                            null,
+                            null,
+                            $href,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+}
