e45fad017550c34d23263c60a8c3f05239189f44
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-09 16:42:21 +0400

    парсер сайта https://kuban.rbc.ru

---
+++ components/parser/news/KubanRbcParser.php
@@ -0,0 +1,126 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class KubanRbcParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'https://kuban.rbc.ru/krasnodar/';
+    private const COUNT = 10;
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('.item__wrap');
+        if (count($items)) {
+            $n = 0;
+            foreach ($items as $item) {
+                if ($n >= self::COUNT) {
+                    break;
+                }
+                try {
+                    $item = PhpQuery::pq($item);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $title = trim($item->find('.item__title')->text());
+                $original = $item->find('.item__link')->attr('href');
+                $originalParser = self::getParser($original, $curl);
+                $createDate = $originalParser->find('.article__header__date')->attr('content');
+                $createDate = date('d.m.Y H:i:s', strtotime($createDate));
+                $image = $item->find('.item__image')->attr('src');
+                $description = $originalParser->find('.article__text.article__text_free p:first')->text();
+                $description = empty($description) ? $title : $description;
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+                $n++;
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $paragraphs = $parser->find('.article__text.article__text_free p:gt(0)');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                $text = trim($paragraph->textContent);
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+        $images = $paragraphs->find('img');
+        if (count($images)) {
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (!empty($src)) {
+                    if (filter_var($src, FILTER_VALIDATE_URL)) {
+                        $post->addItem(
+                            new NewsPostItem(
+                                NewsPostItem::TYPE_IMAGE,
+                                null,
+                                $src,
+                            )
+                        );
+                    }
+                }
+            }
+        }
+        $links = $paragraphs->find('a');
+        if (count($links)) {
+            foreach ($links as $link) {
+                $href = $link->getAttribute('href');
+                if (!empty($href) && filter_var($href, FILTER_VALIDATE_URL)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_LINK,
+                            null,
+                            null,
+                            $href,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+}
