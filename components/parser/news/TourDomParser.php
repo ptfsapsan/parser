86be2a904228e304981e5bcbf82c7327073886c4
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-09 17:05:56 +0400

    парсер сайта https://www.tourdom.ru

---
+++ components/parser/news/TourDomParser.php
@@ -0,0 +1,122 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class TourDomParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'https://www.tourdom.ru/rss/';
+    private const DOMAIN = 'https://www.tourdom.ru';
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('item');
+        if (count($items)) {
+            foreach ($items as $item) {
+                try {
+                    $item = PhpQuery::pq($item);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $title = trim($item->find('title')->text());
+                $original = trim($item->find('link')->text());
+                $createDate = $item->find('pubDate')->text();
+                $createDate = date('d.m.Y H:i:s', strtotime($createDate));
+                $description = trim(strip_tags($item->find('description')->text()));
+                $originalParser = self::getParser($original, $curl);
+                $image = $originalParser->find('.article__img img:first')->attr('src');
+                $image = empty($image) ? null : sprintf('%s%s', self::DOMAIN, $image);
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $paragraphs = $parser->find('article p');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                $text = trim($paragraph->textContent);
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+        $images = $paragraphs->find('img');
+        if (count($images)) {
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (!empty($src)) {
+                    $src = sprintf('%s%s', self::DOMAIN, $src);
+                    if (filter_var($src, FILTER_VALIDATE_URL)) {
+                        $post->addItem(
+                            new NewsPostItem(
+                                NewsPostItem::TYPE_IMAGE,
+                                null,
+                                $src,
+                            )
+                        );
+                    }
+                }
+            }
+        }
+        $links = $paragraphs->find('a');
+        if (count($links)) {
+            foreach ($links as $link) {
+                $href = $link->getAttribute('href');
+                if (!empty($href)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_LINK,
+                            null,
+                            null,
+                            $href,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+}
