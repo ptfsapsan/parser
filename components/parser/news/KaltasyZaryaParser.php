81c6d08a1cb851bdfcad0546bd61b4834bcd94c0
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-09 15:25:26 +0400

    парсер сайта https://kaltasyzarya.rbsmi.ru

---
+++ components/parser/news/KaltasyZaryaParser.php
@@ -0,0 +1,135 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class KaltasyZaryaParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'https://kaltasyzarya.rbsmi.ru/news/';
+    private const DOMAIN = 'https://kaltasyzarya.rbsmi.ru';
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('.main-foto-block-all .main-foto-block');
+        if (count($items)) {
+            foreach ($items as $item) {
+                try {
+                    $item = PhpQuery::pq($item);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $original = $item->find('a.slider2-a')->attr('href');
+                $original = sprintf('%s%s', self::DOMAIN, $original);
+                $title = $item->find('a.slider2-a')->text();
+                $image = $item->find('a.main-foto-img img')->attr('src');
+                $image = sprintf('%s%s', self::DOMAIN, $image);
+                $description = trim($item->find('p')->text());
+                $originalParser = self::getParser($original, $curl);
+                $createDate = $originalParser->find('div.data p')->text();
+                $createDate = sprintf('%s %s:00', trim($createDate), date('H:i'));
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $header = $parser->find('.pin p:first string')->text();
+        if (!empty($header)) {
+            $post->addItem(
+                new NewsPostItem(
+                    NewsPostItem::TYPE_HEADER,
+                    trim($header),
+                    null,
+                    null,
+                    1,
+                )
+            );
+        }
+        $paragraphs = $parser->find('.pin p');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                $text = trim($paragraph->textContent);
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+        $images = $parser->find('.pin p img');
+        if (count($images)) {
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (!empty($src)) {
+                    $src = sprintf('%s%s', self::DOMAIN, $src);
+                    if (filter_var($src, FILTER_VALIDATE_URL)) {
+                        $post->addItem(
+                            new NewsPostItem(
+                                NewsPostItem::TYPE_IMAGE,
+                                null,
+                                $src,
+                            )
+                        );
+                    }
+                }
+            }
+        }
+        $links = $parser->find('.pin p a');
+        if (count($links)) {
+            foreach ($links as $link) {
+                $href = $link->getAttribute('href');
+                if (!empty($href) && filter_var($href, FILTER_VALIDATE_URL)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_LINK,
+                            null,
+                            null,
+                            $href,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+}
