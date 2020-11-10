9771b6363a9b44e264648420498a6ecbc42bbadf
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-10 16:36:08 +0400

    заменил все спец html символы

--- components/parser/news/MGazetaParser.php
+++ components/parser/news/MGazetaParser.php
@@ -90,6 +90,7 @@ class MGazetaParser implements ParserInterface
                 self::setLink($paragraph, $post);
                 $text = htmlentities($paragraph->textContent);
                 $text = trim(str_replace('&nbsp;','',$text));
+                $text = html_entity_decode($text);
                 if (!empty($text)) {
                     $post->addItem(
                         new NewsPostItem(
