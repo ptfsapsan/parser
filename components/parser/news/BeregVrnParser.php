65bf531b613b0d3bb7f23a2daeb2a4f1409a63b4
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-10 13:27:04 +0400

    парсер сайта http://www.bereg.vrn.ru

---
+++ components/parser/news/BeregVrnParser.php
@@ -0,0 +1,152 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use DOMElement;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class BeregVrnParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'http://www.bereg.vrn.ru/31.html';
+    private const DOMAIN = 'http://www.bereg.vrn.ru';
+    private const COUNT = 10;
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('#content .news1');
+        if (count($items)) {
+            $n = 0;
+            foreach ($items as $item) {
+                if ($n >= self::COUNT) {
+                    break;
+                }
+                try {
+                    $item = PhpQuery::pq($item);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $a = $item->find('a');
+                $title = trim($a->text());
+                $original = $a->attr('href');
+                $original = sprintf('%s/%s', self::DOMAIN, $original);
+                $createDate = $item->find('.dt')->text();
+                $d = explode(' / ', $createDate);
+                $createDate = sprintf('%d.%d.%d %s', $d[0], $d[1], $d[2], date('H:i:s'));
+                $originalParser = self::getParser($original, $curl);
+                $description = $originalParser->find('#content p strong:first')->text();
+                $description = empty($description) ? $title : $description;
+                $image = $item->find('table.intr td.image img')->attr('src');
+                $image = empty($image) ? null : sprintf('%s/%s', self::DOMAIN, $image);
+                $image = filter_var($image, FILTER_VALIDATE_URL) ? $image : null;
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+                $n++;
+            }
+        }
+
+        return $posts;
+    }
+
+    /**
+     * @param string $link
+     * @param Curl $curl
+     * @return PhpQueryObject
+     * @throws Exception
+     */
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        $content = $curl->get(Helper::prepareUrl($link));
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $paragraphs = $parser->find('#content p');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                self::setImage($paragraph, $post);
+                self::setLink($paragraph, $post);
+                $text = htmlentities($paragraph->textContent);
+                $text = trim(str_replace('&nbsp;','',$text));
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+    private static function setImage(DOMElement $paragraph, NewsPost $post)
+    {
+        try {
+            $item = PhpQuery::pq($paragraph);
+        } catch (Exception $e) {
+            return;
+        }
+        $src = $item->find('img')->attr('src');
+        if (empty($src)) {
+            return;
+        }
+        if (strpos($src, 'http') === false) {
+            $src = sprintf('%s%s', self::DOMAIN, $src);
+        }
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_IMAGE,
+                null,
+                $src,
+            )
+        );
+    }
+
+    private static function setLink(DOMElement $paragraph, NewsPost $post)
+    {
+        try {
+            $item = PhpQuery::pq($paragraph);
+        } catch (Exception $e) {
+            return;
+        }
+        $href = $item->find('a')->attr('href');
+        if (empty($href)) {
+            return;
+        }
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_LINK,
+                null,
+                null,
+                $href,
+            )
+        );
+    }
+}
