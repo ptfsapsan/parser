fcb3a8c31f4a2fb8660624073a8157ab5efe3a51
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-10 10:21:23 +0400

    парсер сайта https://www.smi.today

---
+++ components/parser/news/SmiTodayParser.php
@@ -0,0 +1,121 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class SmiTodayParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'https://www.smi.today/ru_smi/';
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('#dle-content .shortroot .short');
+        if (count($items)) {
+            foreach ($items as $item) {
+                try {
+                    $item = PhpQuery::pq($item);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $a = $item->find('.short-title a');
+                $title = trim($a->text());
+                $original = $a->attr('href');
+                $originalParser = self::getParser($original, $curl);
+                $date = $originalParser->find('.full-news-panel .pull-right')->text();
+                $date = trim(str_replace('Опубликовано: ', '', $date));
+                $date = substr_replace($date, '20', 6, 0);
+                $time = trim($item->find('.short-data')->text());
+                $createDate = sprintf('%s %s:00', $date, $time);
+                $image = null;
+                $description = $title;
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    var_dump($e->getMessage());
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+
+
+        $text = $parser->find('.newful')->text();
+        if (!empty($text)) {
+            $post->addItem(
+                new NewsPostItem(
+                    NewsPostItem::TYPE_TEXT,
+                    trim($text),
+                )
+            );
+        }
+        $images = $parser->find('.newful img');
+        if (count($images)) {
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (!empty($src) && strpos($src, 'imagelog/realnoevremya.ru') !== false) {
+                    if (filter_var($src, FILTER_VALIDATE_URL)) {
+                        $post->addItem(
+                            new NewsPostItem(
+                                NewsPostItem::TYPE_IMAGE,
+                                null,
+                                $src,
+                            )
+                        );
+                    }
+                }
+            }
+        }
+        $links = $parser->find('.newful a');
+        if (count($links)) {
+            foreach ($links as $link) {
+                $href = $link->getAttribute('href');
+                if (!empty($href) && filter_var($href, FILTER_VALIDATE_URL)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_LINK,
+                            null,
+                            null,
+                            $href,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+}
