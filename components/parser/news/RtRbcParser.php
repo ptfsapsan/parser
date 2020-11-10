197d2198e8e8f218a9db6751bfcb80d968b844ae
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-10 09:35:56 +0400

    парсер сайта https://rt.rbc.ru

---
+++ components/parser/news/RtRbcParser.php
@@ -0,0 +1,128 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use DOMElement;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class RtRbcParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'https://rt.rbc.ru/';
+
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('.js-news-feed-list a');
+        if (count($items)) {
+            foreach ($items as $item) {
+                $title = trim($item->getElementsByTagName('span')->item(0)->textContent);
+                $original = $item->getAttribute('href');
+                if (strpos($original, 'https://www.rbc.ru/') === false) {
+                    continue;
+                }
+                $originalParser = self::getParser($original, $curl);
+                $createDate = $originalParser->find('.article__header__date')->attr('content');
+                $createDate = date('d.m.Y H:i:s', strtotime($createDate));
+                $image = $originalParser->find('.article__main-image__wrap img')->attr('src');
+                $description = $originalParser->find('.article__text.article__text_free p:first')->text();
+                $description = empty($description) ? $title : $description;
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+            }
+        }
+
+        return $posts;
+    }
+
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        try {
+            $content = $curl->get(Helper::prepareUrl($link));
+        } catch (Exception $e) {
+            return null;
+        }
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $paragraphs = $parser->find('.article__text.article__text_free p');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                self::setImage($paragraph, $post);
+                self::setLink($paragraph, $post);
+                $text = trim($paragraph->textContent);
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+    private static function setImage(DOMElement $paragraph, NewsPost $post)
+    {
+        try {
+            $item = PhpQuery::pq($paragraph);
+        } catch (Exception $e) {
+            return;
+        }
+        $src = $item->find('img')->attr('src');
+        if (empty($src)) {
+            return;
+        }
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_IMAGE,
+                null,
+                $src,
+            )
+        );
+    }
+
+    private static function setLink(DOMElement $paragraph, NewsPost $post)
+    {
+        try {
+            $item = PhpQuery::pq($paragraph);
+        } catch (Exception $e) {
+            return;
+        }
+        $href = $item->find('a')->attr('href');
+        if (empty($href)) {
+            return;
+        }
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_LINK,
+                null,
+                null,
+                $href,
+            )
+        );
+    }
+}
