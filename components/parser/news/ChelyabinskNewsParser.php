8de63a182bbd331d5741e9f4912c28e3359cd07c
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-10 13:31:28 +0400

    парсер сайта http://chelyabinsk-news.net

---
+++ components/parser/news/ChelyabinskNewsParser.php
@@ -0,0 +1,156 @@
+<?php
+
+
+namespace app\components\parser\news;
+
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
+use app\components\parser\ParserInterface;
+use DOMElement;
+use Exception;
+use linslin\yii2\curl\Curl;
+use PhpQuery\PhpQuery;
+use PhpQuery\PhpQueryObject;
+
+class ChelyabinskNewsParser implements ParserInterface
+{
+    const USER_ID = 2;
+    const FEED_ID = 2;
+
+    private const LINK = 'http://chelyabinsk-news.net/rss/news';
+    private const COUNT = 10;
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public static function run(): array
+    {
+        $posts = [];
+        $curl = Helper::getCurl();
+        $parser = self::getParser(self::LINK, $curl);
+        $items = $parser->find('item');
+        if (count($items)) {
+            $n = 0;
+            foreach ($items as $item) {
+                if ($n >= self::COUNT) {
+                    break;
+                }
+                $title = trim($item->getElementsByTagName('title')->item(0)->textContent);
+                $original = trim($item->getElementsByTagName('link')->item(0)->nextSibling->textContent);
+                $createDate = trim($item->getElementsByTagName('pubdate')->item(0)->textContent);
+                $createDate = date('d.m.Y H:i:s', strtotime($createDate));
+                $originalParser = self::getParser2($original, $curl);
+                $description = trim($item->getElementsByTagName('description')->item(0)->textContent);
+                $description = str_replace('...', '', $description);
+                $description = empty($description) ? $originalParser->find('[itemprop=articleBody] p:first')->text() : $description;
+                $description = empty($description) ? $title : $description;
+                $image = $originalParser->find('img#current_img')->attr('src');
+                $image = filter_var($image, FILTER_VALIDATE_URL) ? $image : null;
+                try {
+                    $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $posts[] = self::setOriginalData($originalParser, $post);
+                $n++;
+            }
+        }
+
+        return $posts;
+    }
+
+    /**
+     * @param string $link
+     * @param Curl $curl
+     * @return PhpQueryObject
+     * @throws Exception
+     */
+    private static function getParser(string $link, Curl $curl): PhpQueryObject
+    {
+        $content = $curl->get(Helper::prepareUrl($link));
+        $content = mb_convert_encoding($content, 'UTF-8', 'windows-1251');
+        $content = str_replace('<?xml version="1.0" encoding="windows-1251"?>', '', $content);
+
+        return PhpQuery::newDocument($content);
+    }
+
+    /**
+     * @param string $link
+     * @param Curl $curl
+     * @return PhpQueryObject
+     * @throws Exception
+     */
+    private static function getParser2(string $link, Curl $curl): PhpQueryObject
+    {
+        $content = $curl->get(Helper::prepareUrl($link));
+
+        return PhpQuery::newDocument($content);
+    }
+
+    private static function setOriginalData(PhpQueryObject $parser, NewsPost $post): NewsPost
+    {
+        $paragraphs = $parser->find('[itemprop=articleBody] p');
+        if (count($paragraphs)) {
+            foreach ($paragraphs as $paragraph) {
+                self::setImage($paragraph, $post);
+                self::setLink($paragraph, $post);
+                $text = htmlentities($paragraph->textContent);
+                $text = trim(str_replace('&nbsp;','',$text));
+                if (!empty($text)) {
+                    $post->addItem(
+                        new NewsPostItem(
+                            NewsPostItem::TYPE_TEXT,
+                            $text,
+                        )
+                    );
+                }
+            }
+        }
+
+        return $post;
+    }
+
+    private static function setImage(DOMElement $paragraph, NewsPost $post)
+    {
+        try {
+            $item = PhpQuery::pq($paragraph);
+        } catch (Exception $e) {
+            return;
+        }
+        $src = $item->find('img')->attr('src');
+        if (empty($src)) {
+            return;
+        }
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_IMAGE,
+                null,
+                $src,
+            )
+        );
+    }
+
+    private static function setLink(DOMElement $paragraph, NewsPost $post)
+    {
+        try {
+            $item = PhpQuery::pq($paragraph);
+        } catch (Exception $e) {
+            return;
+        }
+        $href = $item->find('a')->attr('href');
+        if (empty($href)) {
+            return;
+        }
+        $post->addItem(
+            new NewsPostItem(
+                NewsPostItem::TYPE_LINK,
+                null,
+                null,
+                $href,
+            )
+        );
+    }
+}
