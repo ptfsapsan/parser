7bfa3f1147571076ae44858a05439ab49d68fc5a
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-11 18:45:51 +0400

    Убрать пробелы по краям текста.

--- components/parser/news/ParkInfoTvParser.php
+++ components/parser/news/ParkInfoTvParser.php
@@ -70,7 +70,9 @@ class ParkInfoTvParser implements ParserInterface
         $paragraphs = $parser->find('.text-inner:gt(0):not(:last) p');
         if (count($paragraphs)) {
             foreach ($paragraphs as $paragraph) {
-                $text = trim($paragraph->textContent);
+                $text = htmlentities($paragraph->textContent);
+                $text = trim(str_replace('&nbsp;','',$text));
+                $text = html_entity_decode($text);
                 if (!empty($text)) {
                     $post->addItem(
                         new NewsPostItem(
