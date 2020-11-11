cb347f532ee0a33f39c01f8aec2eaa67497cf830
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-11 13:15:14 +0400

    Убрать Html символы

--- components/parser/news/MyGazetaParser.php
+++ components/parser/news/MyGazetaParser.php
@@ -87,6 +87,7 @@ class MyGazetaParser implements ParserInterface
                 self::setLink($paragraph, $post);
                 $text = htmlentities($paragraph->textContent);
                 $text = trim(str_replace('&nbsp;','',$text));
+                $text = html_entity_decode($text);
                 if (!empty($text)) {
                     $post->addItem(
                         new NewsPostItem(
