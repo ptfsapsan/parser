46aa8c9c88c8fe9a650ac00e938c675557f3b9c9
Alexey Shirobokov <ashirobokov@i-neti.ru>
2020-11-16 12:36:17 +0400

    исправил

--- components/parser/news/ZarpressaParser.php
+++ components/parser/news/ZarpressaParser.php
@@ -24,6 +24,7 @@ class ZarpressaParser implements ParserInterface
     private const COUNT = 10;
     private const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36 Edg/86.0.622.38';
     private static $description;
+    private static $mainImageSrc;
 
     /**
      * @return array
@@ -53,6 +54,7 @@ class ZarpressaParser implements ParserInterface
                 $originalParser = self::getParser($original, $curl);
                 $description = self::getDescription($originalParser) ?? $title;
                 $image = $originalParser->find('.news-gallery-big img')->attr('src');
+                self::$mainImageSrc = $image;
                 $image = empty($image) ? null : sprintf('%s%s', self::DOMAIN, $image);
                 try {
                     $post = new NewsPost(self::class, $title, $description, $createDate, $original, $image);
@@ -75,7 +77,8 @@ class ZarpressaParser implements ParserInterface
         if (count($paragraphs)) {
             foreach (current($paragraphs->get())->childNodes as $paragraph) {
                 $text = htmlentities($paragraph->textContent);
-                $text = trim(str_replace('&nbsp;', ' ', $text));
+                $text = trim(str_replace('&nbsp;', '', $text));
+                $text = html_entity_decode($text);
                 if (!empty($text)) {
                     self::$description = $text;
                     return $text;
@@ -111,7 +114,8 @@ class ZarpressaParser implements ParserInterface
                     self::setLink($paragraph, $post);
                 }
                 $text = htmlentities($paragraph->textContent);
-                $text = trim(str_replace('&nbsp;', ' ', $text));
+                $text = trim(str_replace('&nbsp;', '', $text));
+                $text = html_entity_decode($text);
                 if (!empty($text) && $text != self::$description) {
                     $post->addItem(
                         new NewsPostItem(
@@ -134,7 +138,7 @@ class ZarpressaParser implements ParserInterface
             return;
         }
         $src = $item->find('img')->attr('src');
-        if (empty($src)) {
+        if (empty($src) || $src == self::$mainImageSrc) {
             return;
         }
         if (strpos($src, 'http') === false) {
